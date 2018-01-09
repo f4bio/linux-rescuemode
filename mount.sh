ecfe932f9f9faeb0f0b81d63f67a01898789c2ee
Fabio Tea <f4bio@users.noreply.github.com>
2018-01-09 02:49:11 +0100

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 read -p "Enter boot partition [/dev/sda1]: " inBootPartition
-read -p "Enter boot partition [/dev/sda3]: " inRootPartition
+read -p "Enter root partition [/dev/sda3]: " inRootPartition
 read -p "Enter mount path [/mnt/custom]: " inMountPath
 read -p "Enter shell path [/bin/bash]: " inShellPath
 
@@ -12,8 +12,8 @@ export SHELLPATH=${inShellPath:-/bin/bash}
 
 mkdir -p "$MOUNTPATH"
 
-mount "$ROOTHDD" "$MOUNTPATH"
-mount "$BOOTHDD" "$MOUNTPATH"/boot
+mount "$ROOTPART" "$MOUNTPATH"
+mount "$BOOTPART" "$MOUNTPATH"/boot
 
 mount --bind /dev "$MOUNTPATH"/dev
 mount --bind /proc "$MOUNTPATH"/proc
